2b4b21801c6a5c0b67b3af4f4b1bf4b166ca9d02
amndazhang <amandazhanglele@gmail.com>
2026-01-17 14:44:12 -0800

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,26 @@
-'use client'
-import { supabase } from '@/lib/supabaseClient'
-
-export default function Login() {
-  const signIn = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: 'google'
-    })
-  }
-
+export default function LoginPage() {
   return (
-    <main>
-      <h1>CHAOS Gear Shed</h1>
-      <button onClick={signIn}>Sign in with CalNet</button>
-    </main>
-  )
+    <div>
+      <h1>Login Page</h1>
+    </div>
+  );
 }
+
+
+// 'use client'
+// import { supabase } from '@/lib/supabaseClient'
+
+// export default function Login() {
+//   const signIn = async () => {
+//     await supabase.auth.signInWithOAuth({
+//       provider: 'google'
+//     })
+//   }
+
+//   return (
+//     <main>
+//       <h1>CHAOS Gear Shed</h1>
+//       <button onClick={signIn}>Sign in with CalNet</button>
+//     </main>
+//   )
+// }
